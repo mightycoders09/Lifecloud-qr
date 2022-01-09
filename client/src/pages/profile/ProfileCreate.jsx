b11b7beaf548be0159beb6f6b008420b1fbba71e
YonatanBenezra <yonatanbenezra1@gmail.com>
2022-01-09 20:44:31 +0200

    profile-creation-page

--- client/src/pages/profile/ProfileCreate.jsx
+++ client/src/pages/profile/ProfileCreate.jsx
@@ -7,8 +7,37 @@ import profiles from './dummy-profiles.json';
 import './profile.css';
 
 export default function ProfileCreate() {
+  const [picture, setPicture] = useState(null);
+  const [imgData, setImgData] = useState(null);
+  const onChangePicture = (e) => {
+    if (e.target.files[0]) {
+      console.log('picture: ', e.target.files);
+      setPicture(e.target.files[0]);
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
+        setImgData(reader.result);
+      });
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  };
+  const [image, setImage] = useState(null);
+  const [coverData, setCoverData] = useState(null);
+  const onChangeCover = (e) => {
+    if (e.target.files[0]) {
+      console.log('picture: ', e.target.files);
+      setImage(e.target.files[0]);
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
+        setCoverData(reader.result);
+      });
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  };
+
+  const [inputList, setInputList] = useState([
+    { axisTitle: '', axisDate: '', axisDescription: '' },
+  ]);
   const [selectedGender, setSelectedGender] = useState('');
-  const [axisAmount, setAxisAmount] = useState(1);
   const firstName = useRef();
   const lastName = useRef();
   const companyName = useRef();
@@ -29,6 +58,27 @@ export default function ProfileCreate() {
   const handleChange = (e) => {
     setSelectedGender(e.target.value);
   };
+
+  // handle input change
+  const handleInputChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...inputList];
+    list[index][name] = value;
+    setInputList(list);
+  };
+
+  // handle click event of the Remove button
+  const handleRemoveClick = (index) => {
+    const list = [...inputList];
+    list.splice(index, 1);
+    setInputList(list);
+  };
+
+  // handle click event of the Add button
+  const handleAddClick = () => {
+    setInputList([...inputList, { firstName: '', lastName: '' }]);
+  };
+
   const handleClick = async (e) => {
     e.preventDefault();
     if (passwordAgain.current.value !== password.current.value) {
@@ -63,27 +113,54 @@ export default function ProfileCreate() {
   return (
     <div className="create-profile-container">
       <Topbar />
-      <div className="login">
+      <div className="profile-creation">
         <div className="loginWrapper">
           <div className="loginLeft">
             <h3 className="loginLogo">Edit Profile</h3>
           </div>
           <div className="profile-images">
+            <div className="register_profile_image"></div>
+            {/* <div className="profile-image-container">
+              <img
+                className="profile-image"
+                src={
+                    !imgData &&
+                    'https://res.cloudinary.com/social-media-appwe/image/upload/v1633782265/social/assets/person/noAvatar_f5amkd.png'
+                }
+                alt=""
+              />
+            </div> */}
             <div className="profile-image-container">
               <img
                 className="profile-image"
-                src={`https://res.cloudinary.com/social-media-appwe/image/upload/v1633782265/social/assets/person/noAvatar_f5amkd.png`}
+                src={
+                  imgData
+                    ? imgData
+                    : `https://res.cloudinary.com/social-media-appwe/image/upload/v1633782265/social/assets/person/noAvatar_f5amkd.png`
+                }
                 alt=""
               ></img>
-              <p>+ Edit Profile image</p>
+              <input
+                className="custom-file-input"
+                type="file"
+                onChange={onChangePicture}
+              />
             </div>
             <div className="profile-image-container">
               <img
                 className="profile-image"
-                src={`https://res.cloudinary.com/social-media-appwe/image/upload/v1633782265/social/assets/person/noAvatar_f5amkd.png`}
+                src={
+                  coverData
+                    ? coverData
+                    : `https://res.cloudinary.com/social-media-appwe/image/upload/v1633782265/social/assets/person/noAvatar_f5amkd.png`
+                }
                 alt=""
               ></img>
-              <p>+ Edit Cover image</p>
+              <input
+                className="custom-file-input"
+                type="file"
+                onChange={onChangeCover}
+              />
             </div>
           </div>
           <div className="loginRight">
@@ -128,7 +205,14 @@ export default function ProfileCreate() {
                       name="gender"
                       className="radio"
                     />
-                    <label for="male">Male</label>
+                    <label
+                      for="male"
+                      className={`${
+                        selectedGender === 'male' && 'active'
+                      } input-label`}
+                    >
+                      Male
+                    </label>
                   </div>
                   <div className="radio-input-container">
                     <input
@@ -139,12 +223,19 @@ export default function ProfileCreate() {
                       name="gender"
                       className="radio"
                     />
-                    <label for="female">Female</label>
+                    <label
+                      for="female"
+                      className={`${
+                        selectedGender === 'female' && 'active'
+                      } input-label`}
+                    >
+                      Female
+                    </label>
                   </div>
                 </div>
                 <div className="location-container">
                   <h1>* Graves Location</h1>
-                  <div>
+                  <div className="location-semicontainer">
                     <div className="names-container">
                       <input
                         placeholder="* Add waze navigation"
@@ -162,19 +253,26 @@ export default function ProfileCreate() {
                   </div>
                 </div>
                 <div className="location-container">
-                  {/* <h1>* Upload photos & videos to gallery</h1>
+                  <h1>* Upload photos & videos to gallery</h1>
                   <div>
                     <div className="names-container">
-                      <input
-                        type="file"
-                        value={selectedFile}
-                        onChange={(e) => setSelectedFile(e.target.files[0])}
-                      />
-                      <img className="img-placeholder"></img>
-                      <img className="img-placeholder"></img>
-                      <img className="img-placeholder"></img>
+                      <div className="register_profile_image">
+                        <input
+                          id="profilePic"
+                          type="file"
+                          onChange={onChangePicture}
+                        />
+                      </div>
+                      <div className="previewProfilePic">
+                        <img
+                          className="playerProfilePic_home_tile"
+                          src={imgData}
+                          alt=""
+                        />
+                      </div>
                     </div>
-                  </div>  to do*/}
+                  </div>{' '}
+                  to do
                 </div>
                 <input
                   placeholder="* Add description of the deceased"
@@ -183,9 +281,56 @@ export default function ProfileCreate() {
                   className="nameInput description"
                 />
                 <div>
-                  <h1>Life Axis</h1>
+                  <h1 style={{ textAlign: 'center' }}>Life Axis</h1>
+                  {inputList.map((x, i) => {
+                    return (
+                      <div className="box">
+                        <div className="inner-box">
+                          <input
+                            name="axisTitle"
+                            placeholder="* Title"
+                            value={x.axisTitle}
+                            onChange={(e) => handleInputChange(e, i)}
+                            className="axis-input"
+                          />
+                          <input
+                            name="axisDate"
+                            placeholder="* Date"
+                            value={x.axisDate}
+                            onChange={(e) => handleInputChange(e, i)}
+                            className="axis-input"
+                          />
+                          <input
+                            name="axisDescription"
+                            placeholder="* Description"
+                            value={x.axisDescription}
+                            onChange={(e) => handleInputChange(e, i)}
+                            className="axis-description"
+                          />
+                          <div className="btn-box">
+                            {inputList.length !== 1 && (
+                              <p
+                                className="delete-btn"
+                                onClick={() => handleRemoveClick(i)}
+                              >
+                                -Delete
+                              </p>
+                            )}
+                          </div>
+                        </div>
+                        {inputList.length - 1 === i && (
+                          <div className="add-btn" onClick={handleAddClick}>
+                            <div className="inner-btn">
+                              <div className="line-1"></div>
+                              <div className="line-2"></div>
+                            </div>
+                          </div>
+                        )}
+                      </div>
+                    );
+                  })}
                 </div>
-                <button className="loginButton" type="submit">
+                <button className="create-btn" type="submit">
                   Save
                 </button>
               </form>
