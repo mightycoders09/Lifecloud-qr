f968e5ece29b32aa508a6e49e3643ee5dafb481c
YonatanBenezra <yonatanbenezra1@gmail.com>
2022-01-10 01:29:12 +0200

    homepage

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -10,16 +10,62 @@ import Rectangle6 from '../../assets/Rectangle6.png';
 import Rectangle7 from '../../assets/Rectangle7.png';
 import Rectangle8 from '../../assets/Rectangle8.png';
 import Rectangle9 from '../../assets/Rectangle9.png';
-import Rectangle12 from '../../assets/Rectangle9.png';
-import Rectangle13 from '../../assets/Rectangle9.png';
-import Rectangle14 from '../../assets/Rectangle9.png';
-import Rectangle15 from '../../assets/Rectangle9.png';
-import Rectangle16 from '../../assets/Rectangle9.png';
-import Rectangle17 from '../../assets/Rectangle9.png';
-
+import Rectangle12 from '../../assets/Rectangle12.png';
+import Rectangle13 from '../../assets/Rectangle13.png';
+import Rectangle14 from '../../assets/Rectangle14.png';
+import Rectangle15 from '../../assets/Rectangle15.png';
+import Rectangle16 from '../../assets/Rectangle16.png';
+import Rectangle17 from '../../assets/Rectangle17.png';
+import standart2 from '../../assets/standart2.png';
+import basic1 from '../../assets/basic1.png';
+import basic2 from '../../assets/basic2.png';
+import Premium1 from '../../assets/Premium1.png';
+import Poster from '../../assets/Poster.png';
+import exampleProfileImage from '../../assets/exampleProfileImage.png';
+import Popup from 'reactjs-popup';
+import { Player } from 'video-react';
+import Slider from 'react-slick';
 import './home.css';
 
 const Home = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: 'linear',
+    lazyLoad: true,
+    speed: 500,
+    slidesToShow: 1,
+    initialSlide: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   const { searchText, setSearchText } = useSearch();
   return (
     <div>
@@ -67,45 +113,366 @@ const Home = () => {
           <strong>using</strong>
         </h1>
       </div>
-      <div>
+      <div className="imgs-container">
         <div>
-          <img src={Rectangle13} alt=""></img>
-          <h1>Social Media</h1>
-          <p>+</p>
-          <p>Read More</p>
+          <Popup
+            trigger={
+              <div>
+                <img className="img-300" src={Rectangle13} alt=""></img>
+                <h1>Social Media</h1>
+                <p>+</p>
+                <p className="button"> Read More </p>
+              </div>
+            }
+            modal
+            nested
+          >
+            {(close) => (
+              <div className="modal">
+                <button className="close" onClick={close}>
+                  &times;
+                </button>
+                <div className="header"> Social Media </div>
+                <div className="content">
+                  {' '}
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Atque, a nostrum. Dolorem, repellat quidem ut, minima sint vel
+                  eveniet quibusdam voluptates delectus doloremque, explicabo
+                  tempore dicta adipisci fugit amet dignissimos?
+                  <br />
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Consequatur sit commodi beatae optio voluptatum sed eius
+                  cumque, delectus saepe repudiandae explicabo nemo nam libero
+                  ad, doloribus, voluptas rem alias. Vitae?
+                </div>
+                <div className="actions">
+                  <button
+                    className="button"
+                    onClick={() => {
+                      console.log('modal closed ');
+                      close();
+                    }}
+                  >
+                    close modal
+                  </button>
+                </div>
+              </div>
+            )}
+          </Popup>
         </div>
         <div>
-          <img src={Rectangle14} alt=""></img>
-          <h1>QR Code</h1>
-          <p>+</p>
-          <p>Read More</p>{' '}
+          <Popup
+            trigger={
+              <div>
+                <img className="img-300" src={Rectangle14} alt=""></img>
+                <h1>QR Code</h1>
+                <p>+</p>
+                <p>Read More</p>{' '}
+              </div>
+            }
+            modal
+            nested
+          >
+            {(close) => (
+              <div className="modal">
+                <button className="close" onClick={close}>
+                  &times;
+                </button>
+                <div className="header"> QR Code </div>
+                <div className="content">
+                  {' '}
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Atque, a nostrum. Dolorem, repellat quidem ut, minima sint vel
+                  eveniet quibusdam voluptates delectus doloremque, explicabo
+                  tempore dicta adipisci fugit amet dignissimos?
+                  <br />
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Consequatur sit commodi beatae optio voluptatum sed eius
+                  cumque, delectus saepe repudiandae explicabo nemo nam libero
+                  ad, doloribus, voluptas rem alias. Vitae?
+                </div>
+                <div className="actions">
+                  <button
+                    className="button"
+                    onClick={() => {
+                      console.log('modal closed ');
+                      close();
+                    }}
+                  >
+                    close modal
+                  </button>
+                </div>
+              </div>
+            )}
+          </Popup>
         </div>
         <div>
-          <img src={Rectangle12} alt=""></img>
-          <h1></h1>
-          <p>+</p>
-          <p>Read More</p>{' '}
+          <Popup
+            trigger={
+              <div>
+                <img className="img-300" src={Rectangle12} alt=""></img>
+                <h1>Saving photos&videos</h1>
+                <p>+</p>
+                <p>Read More</p>{' '}
+              </div>
+            }
+            modal
+            nested
+          >
+            {(close) => (
+              <div className="modal">
+                <button className="close" onClick={close}>
+                  &times;
+                </button>
+                <div className="header"> Saving photos&videos </div>
+                <div className="content">
+                  {' '}
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Atque, a nostrum. Dolorem, repellat quidem ut, minima sint vel
+                  eveniet quibusdam voluptates delectus doloremque, explicabo
+                  tempore dicta adipisci fugit amet dignissimos?
+                  <br />
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Consequatur sit commodi beatae optio voluptatum sed eius
+                  cumque, delectus saepe repudiandae explicabo nemo nam libero
+                  ad, doloribus, voluptas rem alias. Vitae?
+                </div>
+                <div className="actions">
+                  <button
+                    className="button"
+                    onClick={() => {
+                      console.log('modal closed ');
+                      close();
+                    }}
+                  >
+                    close modal
+                  </button>
+                </div>
+              </div>
+            )}
+          </Popup>
         </div>
       </div>
-      <div>
+      <div className="imgs-container">
         <div>
-          <img src={Rectangle16} alt=""></img>
-          <h1></h1>
-          <p>+</p>
-          <p>Read More</p>
+          <Popup
+            trigger={
+              <div>
+                <img className="img-300" src={Rectangle16} alt=""></img>
+                <h1>Wall management</h1>
+                <p>+</p>
+                <p>Read More</p>
+              </div>
+            }
+            modal
+            nested
+          >
+            {(close) => (
+              <div className="modal">
+                <button className="close" onClick={close}>
+                  &times;
+                </button>
+                <div className="header"> Wall management </div>
+                <div className="content">
+                  {' '}
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Atque, a nostrum. Dolorem, repellat quidem ut, minima sint vel
+                  eveniet quibusdam voluptates delectus doloremque, explicabo
+                  tempore dicta adipisci fugit amet dignissimos?
+                  <br />
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Consequatur sit commodi beatae optio voluptatum sed eius
+                  cumque, delectus saepe repudiandae explicabo nemo nam libero
+                  ad, doloribus, voluptas rem alias. Vitae?
+                </div>
+                <div className="actions">
+                  <button
+                    className="button"
+                    onClick={() => {
+                      console.log('modal closed ');
+                      close();
+                    }}
+                  >
+                    close modal
+                  </button>
+                </div>
+              </div>
+            )}
+          </Popup>
         </div>
         <div>
-          <img src={Rectangle17} alt=""></img>
-          <h1></h1>
-          <p>+</p>
-          <p>Read More</p>{' '}
+          <Popup
+            trigger={
+              <div>
+                <img className="img-300" src={Rectangle17} alt=""></img>
+                <h1>Calendar management</h1>
+                <p>+</p>
+                <p>Read More</p>{' '}
+              </div>
+            }
+            modal
+            nested
+          >
+            {(close) => (
+              <div className="modal">
+                <button className="close" onClick={close}>
+                  &times;
+                </button>
+                <div className="header"> Calendar managemen </div>
+                <div className="content">
+                  {' '}
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Atque, a nostrum. Dolorem, repellat quidem ut, minima sint vel
+                  eveniet quibusdam voluptates delectus doloremque, explicabo
+                  tempore dicta adipisci fugit amet dignissimos?
+                  <br />
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Consequatur sit commodi beatae optio voluptatum sed eius
+                  cumque, delectus saepe repudiandae explicabo nemo nam libero
+                  ad, doloribus, voluptas rem alias. Vitae?
+                </div>
+                <div className="actions">
+                  <button
+                    className="button"
+                    onClick={() => {
+                      console.log('modal closed ');
+                      close();
+                    }}
+                  >
+                    close modal
+                  </button>
+                </div>
+              </div>
+            )}
+          </Popup>
         </div>
         <div>
-          <img src={Rectangle15} alt=""></img>
-          <h1></h1>
-          <p>+</p>
-          <p>Read More</p>{' '}
+          <Popup
+            trigger={
+              <div>
+                <img className="img-300" src={Rectangle15} alt=""></img>
+                <h1>Graves locations</h1>
+                <p>+</p>
+                <p>Read More</p>{' '}
+              </div>
+            }
+            modal
+            nested
+          >
+            {(close) => (
+              <div className="modal">
+                <button className="close" onClick={close}>
+                  &times;
+                </button>
+                <div className="header"> Graves locations </div>
+                <div className="content">
+                  {' '}
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Atque, a nostrum. Dolorem, repellat quidem ut, minima sint vel
+                  eveniet quibusdam voluptates delectus doloremque, explicabo
+                  tempore dicta adipisci fugit amet dignissimos?
+                  <br />
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Consequatur sit commodi beatae optio voluptatum sed eius
+                  cumque, delectus saepe repudiandae explicabo nemo nam libero
+                  ad, doloribus, voluptas rem alias. Vitae?
+                </div>
+                <div className="actions">
+                  <button
+                    className="button"
+                    onClick={() => {
+                      console.log('modal closed ');
+                      close();
+                    }}
+                  >
+                    close modal
+                  </button>
+                </div>
+              </div>
+            )}
+          </Popup>
         </div>
+      </div>
+      <Player
+        playsInline
+        poster={Poster}
+        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+        height="50%"
+      />
+      <div className="testimonials">
+        <Slider {...settings}>
+          <div>
+            <h3>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </h3>
+            <h5>-Idan Mor-</h5>
+          </div>
+          <div>
+            <h3>
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s
+            </h3>
+            <h5>-John Smith-</h5>
+          </div>
+          <div>
+            <h3>
+              when an unknown printer took a galley of type and scrambled it to
+              make a type specimen book.
+            </h3>
+            <h5>-Michael Garmin-</h5>
+          </div>
+        </Slider>
+      </div>
+      <div className="example-profile">
+        <a href="#">
+          <img
+            src={exampleProfileImage}
+            alt=""
+            className="example-profile-image"
+          ></img>
+        </a>
+      </div>
+      <a href="/createprofile" className="creation-btn">
+        <div className="profile-div">+ Tap here to create a new profile</div>
+      </a>
+      <div className="plans-section">
+        <h1 className="plans-title">Plans and options for purchase</h1>
+        <div className="plans-container">
+          <div className="plan-container">
+            <img src={basic1} alt=""></img>
+            <h1 className="plan-title">LifePage</h1>
+            <div className="plan-description">
+              <h5>Free</h5>
+              <a>+ For more details</a>
+            </div>
+          </div>
+          <div className="plan-container">
+            <img src={basic2} alt=""></img>
+            <h1 className="plan-title">LifeBook</h1>
+            <div className="plan-description">
+              <h5>1 Year</h5>
+              <a>+ For more details</a>
+            </div>
+          </div>
+          <div className="plan-container">
+            <img src={standart2} alt=""></img>
+            <h1 className="plan-title">LifeBook</h1>
+            <div className="plan-description">
+              <h5>5 Years</h5>
+              <a>+ For more details</a>
+            </div>
+          </div>
+          <div className="plan-container">
+            <img src={Premium1} alt=""></img>
+            <h1 className="plan-title">LifeBook</h1>
+            <div className="plan-description">
+              <h5>Organisation</h5>
+              <a>+ For more details</a>
+            </div>
+          </div>
+        </div>
+        <div className='link-plans'>+ To the plans and purchase page</div>
       </div>
     </div>
   );
