3ffd655e668374e172eda2d7c11411bde177b112
madeel <madeelakhtar204@gmail.com>
2022-01-29 06:13:38 +0500

    privacy update

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,65 +1,72 @@
 const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema(
-    {
-        originalUser: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-        addAdmins: [{
-            user: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-            isAdmin: {
-                type: Boolean
-            }
-        }],
-        // addAdmins: {
-        //     user: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-        //     isAdmin: Boolean
-        // },
-        addFriends: [{
-            user: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-            isFriend: {
-                type: Boolean
-            }
-        }],
-        profileImg: {
-            type: String
+  {
+    originalUser: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    addAdmins: [
+      {
+        user: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+        isAdmin: {
+          type: Boolean,
         },
-        wallImg: {
-            type: String
+      },
+    ],
+    // addAdmins: {
+    //     user: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    //     isAdmin: Boolean
+    // },
+    addFriends: [
+      {
+        user: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+        isFriend: {
+          type: Boolean,
         },
-        graveImg: {
-            type: String,
-        },
-        firstName: {
-            type: String,
-        },
-        lastName: {
-            type: String,
-        },
-        birthDate: {
-            type: Date,
-        },
-        deathDate: {
-            type: Date,
-        },
-        gender: {
-            type: String
-        },
-        wazeLocation: {
-            type: String
-        },
-        googleLocation: {
-            type: String
-        },
-        description: {
-            type: String
-        },
-        gallery: {
-            type: Array
-        },
-        lifeAxis: {
-            type: String
-        },
+      },
+    ],
+    profileImg: {
+      type: String,
     },
-    { timestamps: true }
+    privacy: {
+      type: String,
+    },
+    wallImg: {
+      type: String,
+    },
+    graveImg: {
+      type: String,
+    },
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    birthDate: {
+      type: Date,
+    },
+    deathDate: {
+      type: Date,
+    },
+    gender: {
+      type: String,
+    },
+    wazeLocation: {
+      type: String,
+    },
+    googleLocation: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    gallery: {
+      type: Array,
+    },
+    lifeAxis: {
+      type: String,
+    },
+  },
+  { timestamps: true }
 );
 
 const profileModel = mongoose.model('Profile', PostSchema);
